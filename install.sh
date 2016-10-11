69c282dab524fe708152cd7f3d8d879616ef7d35
Anna Karlsson <anna.karlsson@omegapoint.se>
2016-10-11 15:28:42 +0200

    added lands install-file

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 mvn clean
-mvn install:jars/*.jar -DskipTests
+mvn install:install-file -Dfile=lib/lands-java-lib-0.3-SNAPSHOT.jar -DgroupId=com.github.lands -DartifactId=lands-java-lib -Dversion=0.3-SNAPSHOT -Dpackaging=jar
+mvn install -DskipTests
 mvn compile
