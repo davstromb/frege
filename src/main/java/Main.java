fff1762f753bc58770954c767eb03abfb640a2a8
davstromb <davstromb@gmail.com>
2016-10-11 11:49:12 +0200

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,7 @@
-import frege.prelude.PreludeBase;
-import frege.prelude.PreludeBase.TList.DCons;
-import frege.runtime.Lazy;
-
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList list = new ArrayList<>();
-        Lazy b = null;
 
-
-        FList.TLinkedList
-
-        PreludeBase.TList x = Example.x(7);
-        DCons dCons1 = x._Cons();
-
-
-
-        Integer first = Integer.valueOf(dCons1.mem1.toString());
-
-        System.out.println("-> " + first);
 
 
     }
